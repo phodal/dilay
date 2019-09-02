0163d790045bca171b71ebe29cdf505621a0a5a0
Phodal HUANG <h@phodal.com>
2019-09-02 19:59:05 +0800

    feat: test for class node

---
+++ src/compile/compiler.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from "fs";
+import * as ts from "typescript";
+
+export function delint(sourceFile: ts.SourceFile) {
+  delintNode(sourceFile);
+
+  function delintNode(node: ts.Node) {
+    switch (node.kind) {
+      case ts.SyntaxKind.ClassDeclaration:
+        console.log((node as any)['name']['escapedText']);
+        break;
+
+    }
+
+    ts.forEachChild(node, delintNode);
+  }
+}
+
+const fileNames = process.argv.slice(2);
+fileNames.forEach(fileName => {
+  // Parse a file
+  const sourceFile = ts.createSourceFile(
+    fileName,
+    readFileSync(fileName).toString(),
+    ts.ScriptTarget.ES2015,
+    /*setParentNodes */ true
+  );
+
+  // delint it
+  delint(sourceFile);
+});
