f69fbdb14ab7ac898deeacb8a17f62d1a45c4fa5
Phodal HUANG <h@phodal.com>
2019-09-02 15:53:29 +0800

    feat: add options for test

--- src/cli.ts
+++ src/cli.ts
@@ -8,7 +8,22 @@ let version = require('../../package.json').version;
 
 program
   .version(version)
+  .option('-a, --angular', 'angular style')
+  .option('-r, --react', 'react style')
+  .option('-v, --vue', 'vue style')
+
   .parse(process.argv);
 
+if (program.angular) {
+  console.log('angular');
+}
+
+if (program.react) {
+  console.log('react');
+}
+
+if (program.vue) {
+  console.log('vue');
+}
 
 runDilay();
